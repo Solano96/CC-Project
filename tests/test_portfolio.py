56e805b0496b9589e149b684fd9a72cdb4c323ec
Solano96 <fransol0728@gmail.com>
2019-11-18 12:34:06 +0100

    Mejorando cobertura

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -3,20 +3,25 @@ import sys
 sys.path.append('Portfolio')
 
 from portfolio import Portfolio
+from portfolio import PortfolioException
+
 
 def test_saldo_1():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
     assert portfolio_1.consultar_saldo() == 1500
+
 
 def test_saldo_2():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
     portfolio_1.incrementar_saldo(35)
     assert portfolio_1.consultar_saldo() == 1535
 
+
 def test_saldo_3():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
     portfolio_1.decrementar_saldo(55)
     assert portfolio_1.consultar_saldo() == 1445
+
 
 def test_saldo_4():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
@@ -24,9 +29,20 @@ def test_saldo_4():
     portfolio_1.decrementar_saldo(26)
     assert portfolio_1.consultar_saldo() == 1489
 
+
+def test_saldo_exception_1():
+    portfolio_1 = Portfolio('123123', 'Francisco', 100)
+
+    with pytest.raises(PortfolioException) as excinfo:
+        portfolio_1.decrementar_saldo(200)
+
+    excinfo.match("Error: saldo inferior a la cantidad a substraer.")
+
+
 def test_acciones_1():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
     assert len(portfolio_1.consultar_acciones()) == 0
+
 
 def test_acciones_2():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
@@ -46,6 +62,7 @@ def test_acciones_2():
     assert acciones_googl['mercado'] == "GOOGL"
     assert acciones_googl['acciones'] == 28
 
+
 def test_acciones_3():
     portfolio_1 = Portfolio('123123', 'Francisco', 1500)
     portfolio_1.aniadir_acciones_mercado('GOOGL', 22)
@@ -58,3 +75,25 @@ def test_acciones_3():
 
     assert acciones_googl['mercado'] == "GOOGL"
     assert acciones_googl['acciones'] == 24
+
+
+def test_acciones_exception_1():
+    portfolio_1 = Portfolio('123123', 'Francisco', 1500)
+
+    with pytest.raises(PortfolioException, match="Error: No hay acciones compradas de este mercado."):
+        assert portfolio_1.consultar_acciones_mercado('AAPL')
+
+
+def test_acciones_exception_2():
+    portfolio_1 = Portfolio('123123', 'Francisco', 123)
+
+    with pytest.raises(PortfolioException, match="Error: No hay acciones compradas de este mercado."):
+        assert portfolio_1.substraer_acciones_mercado('AAPL', 20)
+
+
+def test_acciones_exception_3():
+    portfolio_1 = Portfolio('123123', 'Francisco', 123)
+    portfolio_1.aniadir_acciones_mercado('GOOGL', 22)
+
+    with pytest.raises(PortfolioException, match="Error: no se pueden susbtraer más acciones de las que se disponen."):
+        assert portfolio_1.substraer_acciones_mercado('GOOGL', 30)
