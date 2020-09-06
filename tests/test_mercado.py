fdfcb519e9123b2dd8e5e386c23e80f0b9c8e50d
Solano96 <fransol0728@gmail.com>
2020-09-06 11:52:03 +0200

    Corregido error en test

--- tests/test_mercado.py
+++ tests/test_mercado.py
@@ -6,8 +6,8 @@ from Mercado.mercado import Mercado
 
 def test_get_data_1():
     data = Mercado.get_data('AAPL', '2019-11-04', '2019-11-16')
-    assert data['2019-11-04']['Open'] == 257.33
-    assert data['2019-11-15']['Close'] == 265.76
+    assert data['2019-11-04']['Open'] == 64.33
+    assert data['2019-11-15']['Close'] == 66.44
 
 
 def test_get_data_in_realtime():
